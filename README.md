e001afbedf0937a6a07fcf9c7947df30aeeec1c3
Craig Johnson <craigdev937@gmail.com>
2023-12-18 14:58:36 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Marvel API and RTK-Query
+
+Initial setup of the project.  Created with TypeScript, React, React Router, Redux-Toolkit featuring RTK-Query.
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>Marvel API</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur praesentium cum eos expedita amet, minus corrupti ducimus adipisci, minima ab laborum, dignissimos possimus? Distinctio, at ea? Beatae assumenda itaque ducimus.</p>
+        </React.Fragment>
+    );
+};
+
+
